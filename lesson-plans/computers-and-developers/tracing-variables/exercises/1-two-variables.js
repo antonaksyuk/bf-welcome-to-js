3dbdbedb23534d7871fd01054b9653bc9313890a
antonaksyuk <antonaksyuk@gmail.com>
2025-02-10 21:16:47 +0100

    exercises

--- lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/1-two-variables.js
@@ -9,16 +9,22 @@
 
 */
 
-// trace the program a few times with different values
+// trace the program a few times with diffnpm run study
+// erent values
 
+//declaration variable "x" & initialisation it as a string 'Pieter' or 'Blake' or 'George'
 let x = 'Pieter';
 // let x = 'Blake';
 // let x = 'George';
 
+// declaration variable "y", initialisation it as a string & concatinate it with variable "x" - 'hello '+'Pieter'
 let y = 'hello ' + x;
 
+//output value "y" to the browser's console - 'hello Pieter'
 console.log(y);
 
+// reassign variable "y", initialisation it as a string & concatinate it with variable "x" - 'good bye '+'Pieter'
 y = 'good bye ' + x;
 
+//output value "y" to the browser's console - 'good bye Pieter'
 console.log(y);
--- lesson-plans/computers-and-developers/tracing-variables/exercises/3-hello-good-bye.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/3-hello-good-bye.js
@@ -5,16 +5,23 @@
   Add comments and logs to help a developer understand this program
 
 */
-
+//declaration variable "first" & initialisation it as a string - 'Pieter'
 let first = 'Pieter';
+
+//declaration variable "last" & initialisation it as a string - 'Rabbit'
 let last = 'Rabbit';
 
+//declaration variable "fullName" & initialisation it as a concotination of tree strings - 'Pieter'+' '+'Rabbit'
 let fullName = first + ' ' + last;
 
+//output value "fullName" to the browser's console - 'Pieter Rabbit' 
 console.log(fullName);
 
+// declaration variable "greeting", initialisation it as a string & concatinate it with variable "fullName" - 'hello '+'Pieter Rabbit'
 let greeting = 'hello ' + fullName;
 
+// reassign variable "greeting", initialisation it as a string 'good bye ' & concatinate it with variable "fullName" - 'good bye '+'Pieter Rabbit'
 greeting = 'good bye ' + fullName;
 
+//output value "fgreeting" to the browser's console - 'good bye Pieter Rabbit' 
 console.log(greeting);
